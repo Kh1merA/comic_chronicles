6bc16a717fa2fa325fc6d6e7ef8cf936a6701552
KPas13 <kpasenko64@gmail.com>
2024-01-30 22:08:11 +0200

    update scss file

--- src/components/login_page/LoginPage.js
+++ src/components/login_page/LoginPage.js
@@ -2,8 +2,8 @@ import '../../styles/css/login_page.css';
 export default function LoginPage() {
     return (
         <div>
-            <h1>VI VSE GANDONY I VAS NENAVIZHU</h1>
-            <p>IBIKS THE BEST</p>
+            <h1>Login</h1>
+            <p>Enter your username</p>
         </div>
     )
 }
